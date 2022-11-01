9af54e7447b43cc0181f4eb6d9cedc08f4147853
ataSky <tr.umitatay@gmail.com>
2022-11-01 15:39:12 +0300

    Update inspytu.sh

--- inspytu.sh
+++ inspytu.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 # This script will install specific versions of Python on Ubuntu Linux servers
 if [ "$(whoami)" != "root" ]; then
-    echo "WARNING: You must run this script as 'root'"
+    echo "UYARI: Bu betiği 'root' olarak çalıştırmalısınız."
     # This shortcut method will fail if the script file does not have execute permissions
     exec sudo -- "$0" "$@"
 fi
 
+NPROC=$(nproc)
+echo $NPROC
+
+
 inputVersion=$1
 if [[ $inputVersion == "" ]]; then
-    echo "You must input a supported version (e.g. 2.7.11, 3.5.1)"
+    echo "Desteklenen bir sürüm girmelisiniz (e.g. 2.7.11, 3.5.1)"
     exit 1
 fi
 
 exitOnError() {
     error=$1
     lineno=$2
-    echo "The previous command resulted in a fatal error: ${error}, line: ${lineno}"
+    echo "Önceki komut önemli bir hatayla sonuçlandı: ${error}, line: ${lineno}"
     exit $error
 }
 
@@ -26,15 +30,16 @@ setNameAndURL() {
     pythonURL="https://www.python.org/ftp/python/${inputVersion}/Python-${inputVersion}.tgz"
 }
 
+
 pythonDependencies() {
     /usr/bin/apt-get update -q
     /usr/bin/apt-get install build-essential checkinstall -qqy || exitOnError $?
-    /usr/bin/apt-get install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -qqy
+    /usr/bin/apt-get install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev g++  -qqy
 }
 
 installPip() {
-    /usr/bin/apt-get install build-essential python-dev python-pip -qqy || exitOnError $?
-    /usr/bin/pip install virtualenv || exitOnError $?
+    /usr/bin/apt-get install build-essential python3-dev python3-pip -qqy || exitOnError $?
+    /usr/bin/pip3 install virtualenv || exitOnError $?
 }
 
 download() {
@@ -46,7 +51,7 @@ download() {
 makePython() {
     cd /usr/src/${pythonName}
     ./configure
-    make
+    make -j $(nproc)
     make install
 }
 
